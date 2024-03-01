6a37cd5ec28e24a2f02a3bb1a86fb35e063ed069
nahaksofttech <nahaksofttechnology@gmail.com>
2024-03-01 09:32:45 +0530

    uploading
    yes

---
+++ index.php
@@ -0,0 +1,199 @@
+
+
+
+<!DOCTYPE html>
+<html  >
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v5.2.0, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo-trademark-removebg-preview-336x256.png" type="image/x-icon">
+  <meta name="description" content="Wire Injection Technologies is engaged in the manufacturing of Metallurgical Cored Wire for the steel and metal industry.">
+  
+  
+  <title>Wire Injection Technologies : Home</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+  
+</head>
+<body>
+  
+  <section class="menu cid-shOJYiJRwL" once="menu" id="menu2-0">
+
+    
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm bg-color transparent">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="index.html">
+                        <img src="assets/images/logo-trademark-removebg-preview-336x256.png" alt="WireInjection Technologies" title="" style="height: 8rem;">
+                    </a>
+                </span>
+                
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true"><li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="about.html">
+                        About Us
+                    </a>
+                </li>
+                <li class="nav-item dropdown"><a class="nav-link link text-white dropdown-toggle display-4" href="Services.html" data-toggle="dropdown-submenu" aria-expanded="false" target="_blank">
+                        Services</a><div class="dropdown-menu"><a class="text-white dropdown-item display-4" href="Cored Wire.html">Cored Wire</a><a class="text-white dropdown-item display-4" href="Agglomeration.html">Agglomeration</a></div></li><li class="nav-item"><a class="nav-link link text-white display-4" href="Contact.html">
+                        Contacts</a></li></ul>
+            <div class="navbar-buttons mbr-section-btn"><a class="btn btn-sm btn-primary-outline display-4" href="mailto:wireinjtech@gmail.com">wireinjtech@gmail.com</a></div>
+        </div>
+    </nav>
+</section>
+
+<section class="header6 cid-shOYUZm0DC mbr-fullscreen" data-bg-video="https://www.youtube.com/watch?v=5nLM8fE5EEs&amp;feature=youtu.be&amp;ab_channel=ViruPatelhttps://www.youtube.com/watch?v=5nLM8fE5EEs&amp;feature=youtu.be&amp;ab_channel=ViruPatelhttps://www.youtube.com/watch?v=5nLM8fE5EEs&amp;feature=youtu.be&amp;ab_channel=ViruPatelhttps://www.youtube.com/watch?v=5nLM8fE5EEs&amp;feature=youtu.be&amp;ab_channel=ViruPatelhttps://www.youtube.com/watch?v=5nLM8fE5EEs&amp;feature=youtu.be&amp;ab_channel=ViruPatel" id="header6-3">
+
+    
+
+    <div class="mbr-overlay" style="opacity: 0.6; background-color: rgb(35, 35, 35);">
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="mbr-white col-md-10">
+                
+                <p class="mbr-text align-center pb-3 mbr-fonts-style display-2">
+                    Welcome To Wire Injection Technologies Pvt. Ltd.</p>
+                
+            </div>
+        </div>
+    </div>
+
+    <div class="mbr-arrow hidden-sm-down" aria-hidden="true">
+        <a href="#next">
+            <i class="mbri-down mbr-iconfont"></i>
+        </a>
+    </div>
+</section>
+
+<section class="mbr-section article content9 cid-shP0WGwDfS" id="content9-6">
+    
+     
+
+    <div class="container">
+        <div class="inner-container" style="width: 100%;">
+            <hr class="line" style="width: 25%;">
+            <div class="section-text align-center mbr-fonts-style display-5">Wire Injection Technologies comprises of a team of highly experienced
+ personnel who are experts in the field of metallurgy and product applications. The experience and knowledge of our team thrives us harder to push for the limits towards product development and improvement of our business through client satisfaction.&nbsp;<span style="font-size: 1.5rem;">&nbsp;</span><br><span style="font-size: 1.5rem;"><br></span><div>
+</div><div>Our team of experienced engineers have mastered the art of ladle refining and
+ trimming in the field of cored wire use through continuous learning and adaption to different metallurgical requirements in cored wire addition. We will always seek to provide the most technologically advanced solutions for customers.</div></div>
+            <hr class="line" style="width: 25%;">
+        </div>
+        </div>
+</section>
+
+<section class="cid-sRLopws8xs" id="image1-1x">
+
+    
+
+    <figure class="mbr-figure container">
+        <div class="image-block" style="width: 20%;">
+            <img src="assets/images/th.jpg" width="1400" alt="Mobirise">
+            
+        </div>
+    </figure>
+</section>
+
+<section class="mbr-section contacts1 cid-sSeDT0HVqi" id="contacts1-22">
+    <!---->
+    
+    <!---->
+    <!--Overlay-->
+    
+    <!--Container-->
+    <div class="container">
+        <div class="row">
+            <!--Titles-->
+            <div class="title col-12">
+                <h2 class="align-left mbr-fonts-style display-1">
+                    Our Contacts
+                </h2>
+                
+            </div>
+            <!--Left-->
+            <div class="col-12 col-md-6">
+                <div class="left-block wrapper">
+                    <div class="b b-adress">
+                        <h5 class="align-left mbr-fonts-style m-0 display-5">
+                            Address:
+                        </h5>
+                        <p class="mbr-text align-left mbr-fonts-style display-7">HEAD OFFICE &amp; WORKS : Plot No - 17/I &amp; 17/R, Heavy Industrial Area Hathkhoj, Bhilai,
+                        <br>Dist - Durg 
+<br>Chhattisgarh - 490026. 
+<br>
+<br>CITY OFFICE : B-199, Second Floor, Shakuntala Complex, Bhilai,
+<br>Dist - Durg 
+<br>Chhattisgarh - 490020. 
+<br>
+<br>REGIONAL OFFICE : D1/50 SVP Nagar, Near Mhada Telephone Exchange, 4 Bungalows,
+<br> Andheri (West), Mumbai 
+<br>Maharashtra - 400053.</p>
+                    </div>
+                    <div class="b b-phone">
+                        <h5 class="align-left mbr-fonts-style m-0 display-5">
+                            Phone:
+                        </h5>
+                        <p class="mbr-text align-left mbr-fonts-style display-7"><a href="tel:+91 92291-20003 " class="text-primary">+91 92291-20003 </a>
+<br><a href="tel:+91 79745-98080" class="text-primary">+91 79745-98080</a></p>
+                    </div>
+                    <div class="b b-mail">
+                        <h5 class="align-left mbr-fonts-style m-0 display-5">
+                            E-mail:
+                        </h5>
+                        <p class="mbr-text align-left mbr-fonts-style display-7">
+                            <a href="mailto:wireinjtech@gmail.com" class="text-primary">wireinjtech@gmail.com</a>, <a href="mailto:marketing@wireinjectiontechnologies.com" class="text-primary">marketing@wireinjectiontechnologies.com</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!--Right-->
+            <div class="col-12 col-md-6">
+                <div class="google-map"><iframe frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDk89J4FSunMF33ruMVWJaJht_Ro0kvoXs&amp;q=Plot No - 17, 1A, Heavy Industrial Area Hathkhoj, Hathkhoj, Chhattisgarh 490024" allowfullscreen=""></iframe></div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section once="footers" class="cid-shP3oxUOVf" id="footer6-8">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row align-center mbr-white">
+            <div class="col-12">
+                <p class="mbr-text mb-0 mbr-fonts-style display-7">
+                    Copyright © 2020 | Wire Injection Technologies Inc. | Website Designed by <a href="http://www.klyia.com/">Klyia Technology</a></p>
+            </div>
+        </div>
+    </div>
+</section><section style="background-color: #fff; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Helvetica Neue', Arial, sans-serif; color:#aaa; font-size:12px; padding: 0; align-items: center; display: flex;"><a href="https://mobirise.site/t" style=""></a><p style="flex: 0 0 auto; margin:0; padding-right:1rem;"><a href="https://mobirise.site/w" style="color:#aaa;"></a></p></section><script src="assets/web/assets/jquery/jquery.min.js"></script>  <script src="assets/popper/popper.min.js"></script>  <script src="assets/bootstrap/js/bootstrap.min.js"></script>  <script src="assets/tether/tether.min.js"></script>  <script src="assets/smoothscroll/smooth-scroll.js"></script>  <script src="assets/dropdown/js/nav-dropdown.js"></script>  <script src="assets/dropdown/js/navbar-dropdown.js"></script>  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>  <script src="assets/ytplayer/jquery.mb.ytplayer.min.js"></script>  <script src="assets/vimeoplayer/jquery.mb.vimeo_player.js"></script>  <script src="assets/theme/js/script.js"></script>  
+  
+  
+</body>
+</html>
